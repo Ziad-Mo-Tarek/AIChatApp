e766d6c92137844d624906003bb2d27e3f07db2d
ziad <z.ziad.tarek.t@gmail.com>
2026-04-05 11:29:15 +0200

    Initial Commit

---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by Ziad Tarek on 05/04/2026.
+//
+
+import Testing
+@testable import AIChatCourse
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
